6635fdffe6da6d41671be1bdc21583504b56f4c0
Songhareem <elaha00@naver.com>
2020-04-03 12:38:35 +0900

    jQuery done

--- Study_jQuery/WebContent/js/selectorEx2.js
+++ Study_jQuery/WebContent/js/selectorEx2.js
@@ -2,13 +2,27 @@
  * 
  */
 
-var addLimit = 1;
+var addLimit = 0;
+var index = 0;
+var divId = [];
 $("#add").click(function() {
 
 	if (addLimit === 5) {
 		alert("file은 5개까지");
 		return;
 	}
-	$("#fileDiv").append(`<br><input type="file">`);
+	
+	var tmpId = `d${index}`;
+	divId.push("d" + index);
+	$("#fileDiv").append(`<div id=${tmpId}><input type="file"><span class="r" title=${tmpId}>X</span></div>`);
 	addLimit++;
+	index++;
+});
+
+$("#fileDiv").on("click",".r", function() {
+	
+	console.log($(this));
+	var id = $(this).attr("title");
+	$(`#${id}`).remove();
+	addLimit--;
 });
